dcf26cf30148b90c858f8ec364db5b2c55b884b1
Kyle Randall <kyle.randall@xtratyme.com>
2020-03-03 17:31:52 -0600

    test

---
+++ Practice/Factory/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Factory
+{
+    public class Document
+    {
+        List<Page> aListOfPages;
+
+        public void Add(Page p)
+        {
+            this.aListOfPages.Add(p);
+        }
+    }
+}
---
+++ Practice/Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Singleton
+{
+
+    public sealed class Player
+    {
+        // Class Variables
+        private static readonly Player instance;
+        public string Name { get; set; }
+
+        // Gets and Sets
+        public static Player Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        // Constructors start here
+        private Player() { }
+
+
+        static Player()
+        {
+            instance = new Player()
+            {
+                Name = "The One and Only Radcliff"
+            };
+        }
+  
+
+
+    }
+}
